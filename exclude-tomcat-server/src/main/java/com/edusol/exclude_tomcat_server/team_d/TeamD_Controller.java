ef0d352746932792318b97196ab3a1fc089a6df5
amit07bti <amitkumar.developer764@gmail.com>
2025-09-19 09:30:56 +0530

    TeamD

---
+++ exclude-tomcat-server/src/main/java/com/edusol/exclude_tomcat_server/team_d/TeamD_Controller.java
@@ -0,0 +1,11 @@
+package com.edusol.exclude_tomcat_server.team_d;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamD_Controller {
+
+    public  String teamD(){
+        return "hello Team D";
+    }
+}
